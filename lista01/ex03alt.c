91c7f819d382c2198d68d63e3dbadcebc334c2ee
fe-fe <119269054+fe-fe@users.noreply.github.com>
2024-09-14 11:53:05 -0300

    Create ex03alt.c
    exercicio 03 alterado

---
+++ lista01/ex03alt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faça um programa que apresente os números de 1 a 15 na forma de um triangulo, como
+apresentado a seguir:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15 */
+
+int main(int argc, char *argv[])
+{
+    int ultimo = 1;
+
+    if (argc >= 2)
+    {
+        ultimo = atoi(argv[1]); // funcao que transforma a string do argv[1] em inteiro
+    }
+
+    int max = 1;  // valor maximo que cada linha pode ter, comeca com 1 pois na primeira linha escrevemos apenas 1 valor
+    int casa = 0; // segue a quantidade de numeros que ja foram escritos em 1 linha, e nao pode ultrapassar o valor max
+
+    for (int i = 1; i < ultimo; i++)
+    {
+        printf("%d ", i);
+        casa++;
+        if (casa == max)
+        {
+            printf("\n");
+            casa = 0;
+            max++;
+        }
+    }
+}
